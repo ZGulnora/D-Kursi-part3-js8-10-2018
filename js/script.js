ed2f9a0e72a4e2e4cb0ccafd10dca20b7e6334f8
guli <gulisha64@mail.ru>
2018-10-16 20:55:14 +0500

    Домашка по 8-му уроку

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// Домашка по уроку №8 1-ое задание
+var goods = [];
+goods[30]="Printer";
+alert(goods.length-1);
+//2-ое задание
+goods.push("Компьютер");
+goods.unshift("Monitor");
+
+alert(goods);
+
+
+
+
+// var massiv = ["нексия", "Каптива", "Малибу"];
+// // alert(massiv[1]);
+// massiv[3] = "Cobolt";
+// alert(massiv.length);
+
+// var qwerty = [1, 'Ctroka', {name: 'Petya'}, true];
+// alert(qwerty[2].name);
+
+var massiv = ["нексия", "Каптива", "Малибу", "Cobolt", "Spark"];
+// function eat(name) {
+// 	name.pop();
+// }
+// alert(massiv.length);
+// eat(massiv);
+
+// alert(massiv.length);
+// eat(massiv);
+// alert(massiv.length);
+
+// for (var i = 0; i<massiv.length; i++) {
+// 	var a =massiv[i];
+// 	alert(a);
+// }
+
+// massiv.length = 3;
+// alert(massiv);
+
+// alert(massiv.includes("Каптива"));
+// alert(massiv.includes("Gas")); 
